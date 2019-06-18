43c7b7762e6cf4d73d28122aa809625599202e82
FatemeSafari <Fateme.safari1989@gmail.com>
2019-06-18 16:48:49 +0430

    start

---
+++ 02-Personal Website Responsive/js/myjquery.js
@@ -0,0 +1,7 @@
+// JavaScript source code
+//paste this code under head tag or in a separate js file.
+// wait for window load
+$(window).load(function () {
+    // Animate loader off screen
+    $(".se-pre-con").fadeOut("slow");;
+});
